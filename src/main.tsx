42516b0f0e360076ddd5ab4015a3a110619c0c10
Edrian Miguel Capistrano <edrianmiguelcapistrano@gmail.com>
2026-07-05 23:01:12 +0800

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './styles.css';
 import { App } from './ui/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
